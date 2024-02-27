c1f9d802d10e48edd08c37c36028531d15220dfa
Tran duc <tranhongduc2203@gmail.com>
2024-02-27 19:02:33 +0700

    chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get "demo_partials/edit"
   get "static_pages/home"
   get "static_pages/help"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create show)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
